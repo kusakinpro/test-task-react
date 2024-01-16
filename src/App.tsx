8b6d51223d50235e456a85fbc1be3ce2097afe82
Vladimir <kusakinvova@gmail.com>
2024-01-16 15:59:39 +0300

    init: start task

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+import List from './List';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello!</h1>
+      <List itemsCount={200} />
+    </div>
+  );
+}
